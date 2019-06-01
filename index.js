3c90ea607ca10dacc45982d93a160280a9fd24d9
Jacob Angulo <jacobangulo@hotmail.com>
2019-06-01 04:45:53 -0700

    pushing most recent changes for deployment purposes

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
 const { withUiHook, htm } = require("@zeit/integration-utils");
 
+// const PizzaForm = id => htm`
+//         <Container>
+//             <Input/>
+//         </Container>
+// `;
+
 module.exports = withUiHook(async ({ payload, zeitClient }) => {
   const { clientState, action } = payload;
   const store = await zeitClient.getMetadata();
 
   if (action === "submit") {
-    store.secretId = clientState.secretId;
-    store.secretKey = clientState.secretKey;
-    await zeitClient.setMetadata(store);
+    axios.get("https://a-p-izza.herokuapp.com/");
+    // store.secretId = clientState.secretId;
+    // store.secretKey = clientState.secretKey;
+    // await zeitClient.setMetadata(store);
   }
 
-  if (action === "reset") {
-    store.secretId = "";
-    store.secretKey = "";
-    await zeitClient.setMetadata(store);
-  }
+  //   if (action === "reset") {
+  //     store.secretId = "";
+  //     store.secretKey = "";
+  //     await zeitClient.setMetadata(store);
+  //   }
 
   return htm`
 		<Page>
-			<Container>
-				<Input label="Secret Id" name="secretId" value=${store.secretId || ""} />
-				<Input label="Secret Key" name="secretKey" type="password" value=${store.secretKey ||
-          ""} />
-			</Container>
-			<Container>
-				<Button action="submit">Submit</Button>
-				<Button action="reset">Reset</Button>
-			</Container>
+        <Container>
+        <Button action="test">Submit</Button>
+        </Container>
 		</Page>
-	`;
+        `;
 });
+//     <Container>
+//         <H1>hungry?</H1>
+//         <Input label="Secret Id" name="secretId" value=${store.secretId || ""} />
+//         <Input label="Secret Key" name="secretKey" type="password" value=${store.secretKey ||
+//   ""} />
+//     </Container>
