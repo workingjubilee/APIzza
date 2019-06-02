3b0aaa8d93745fd67d4185d3e8dcae6df01c4c84
Jacob Angulo <jacobangulo@hotmail.com>
2019-06-01 18:15:31 -0700

    working build - hits the endpoint but does not get anything backbac

--- index.js
+++ index.js
@@ -1,59 +1,41 @@
+const { withUiHook } = require("@zeit/integration-utils");
+const axios = require("axios");
 
+module.exports = withUiHook(async options => {
+  const { payload, zeitClient } = options;
+  const { action, clientState } = payload;
+  let metadata = await zeitClient.getMetadata();
 
-// const { withUiHook, htm } = require("@zeit/integration-utils");
-// // const fetch = require('@zeit/fetch')(require('node-fetch'));
-// const axios = require("axios");
-// const fetch = require("@zeit/fetch")(require("node-fetch"));
-// // const PizzaForm = id => htm`
-// //         <Container>
-// //             <Input/>
-// //         </Container>
-// // `;
+  switch (action) {
+    case "submit":
+      metadata = clientState;
+      await zeitClient.setMetadata(metadata);
+    case "reset":
+      metadata = {};
+      await zeitClient.setMetadata(metadata);
+    case "test":
+      metadata.secretId = "test";
+      axios.get("https://a-p-izza.herokuapp.com/").then(test => {
+        console.log(test);
+        return `<H2>${test.body.message}</H2>`;
+      });
+    default:
+      null;
+  }
 
-// module.exports = withUiHook(async ({ payload, zeitClient }) => {
-//   const { clientState, action } = payload;
-//   const store = await zeitClient.getMetadata();
-
-//   if (action === "test") {
-//     // const res = await axios.get("https://a-p-izza.herokuapp.com");
-//     await fetch("https://api.zeit.co/v2/oauth/access_token", {
-//       method: "GET"
-//       // headers: {
-//       //   "Content-Type": "application/x-www-form-urlencoded"
-//       // },
-//       // body: qs.stringify({
-//       //   client_id: CLIENT_ID,
-//       //   client_secret: CLIENT_SECRET,
-//       //   code: query.code,
-//       //   redirect_uri: REDIRECT_URL
-//       // })
-//     });
-
-//     console.log("makes it here");
-//     console.log(res);
-//     // store.secretId = clientState.secretId;
-//     // store.secretKey = clientState.secretKey;
-//     // await zeitClient.setMetadata(store);
-//   }
-
-//   //   if (action === "reset") {
-//   //     store.secretId = "";
-//   //     store.secretKey = "";
-//   //     await zeitClient.setMetadata(store);
-//   //   }
-
-//   return htm`
-//     <Page>
-//         <Container>
-//             <Button action="test">test</Button>
-//             <Button>another button</Button>
-//         </Container>
-// 		</Page>
-//     `;
-// });
-// //     <Container>
-// //         <H1>hungry?</H1>
-// //         <Input label="Secret Id" name="secretId" value=${store.secretId || ""} />
-// //         <Input label="Secret Key" name="secretKey" type="password" value=${store.secretKey ||
-// //   ""} />
-// //     </Container>
+  return `
+        <Page>
+            <Container>
+                <Input label="Secret Id" name="secretId" value="${metadata.secretId ||
+                  ""}"/>
+                <Input label="Secret Key" name="secretKey" type="password" value="${metadata.secretKey ||
+                  ""}" />
+            </Container>
+            <Container>
+                <Button action="submit">Submit</Button>
+                <Button action="reset">Reset</Button>
+                <Button action="test">Test</Button>
+            </Container>
+        </Page>
+    `;
+});
