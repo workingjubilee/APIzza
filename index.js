f610d7f5390c0285149dda4538ac39a5aecaed24
Jacob Angulo <jacobangulo@hotmail.com>
2019-06-02 23:40:12 -0700

    more changes

--- index.js
+++ index.js
@@ -1,107 +1,56 @@
-const micro = require('micro');
-// const fetch = require('@zeit/fetch')(require('node-fetch'));
-const {withUiHook, htm, ZeitClient} = require('@zeit/integration-utils');
-// const {parse: parseUrl} = require('url');
-// const qs = require('querystring');
-const {
-	CLIENT_ID,
-	CLIENT_SECRET,
-	ROOT_URL,
-	REDIRECT_URL
-} = require('./constants.js');
+const micro = require("micro");
+const { withUiHook, htm, ZeitClient } = require("@zeit/integration-utils");
 
-// Use a real database for these stores for production
+const {
+  CLIENT_ID,
+  CLIENT_SECRET,
+  ROOT_URL,
+  REDIRECT_URL
+} = require("./constants.js");
 
 const store = {
   secretId: "",
   secretKey: ""
 };
 
-const renderUIHook = withUiHook(({payload}) => {
-	const {user, team, configurationId} = payload;
-	const ownerId = team? team.id : user.id;
-	const scope = team? team.slug : user.username;
-	const events = EventsStore[ownerId] || [];
+const renderUIHook = withUiHook(async ({ payload }) => {
+  const { user, team, configurationId } = payload;
+  const ownerId = team ? team.id : user.id;
+  const scope = team ? team.slug : user.username;
+  const events = EventsStore[ownerId] || [];
+  const store = await zeitClient.getMetadata();
 
-	if (payload.action === "reset") {
-		store.secretId = "";
-		store.secretKey = "";
-	} else if (payload.action === "test") {
-		store.secretId = "test";
-		axios.get("https://a-p-izza.herokuapp.com/").then(() => `<H2>'works</H2>`);
-	}
+  if (payload.action === "reset") {
+    store.addressLine1 = "";
+    store.addressLine2 = "";
+    store.city = "";
+    store.state = "";
+    store.zip = "";
+    await zeitClient.setMetadata(store);
+  } else if (payload.action === "test") {
+    store.secretId = "test";
+    axios.get("https://a-p-izza.herokuapp.com/").then(() => `<H2>'works</H2>`);
+  }
 
-	return htm`
-        <Page>
-            <Container>
-                <Input label="Secret Id" name="secretId" value="${store.secretId ||
-                  ""}"/>
-                <Input label="Secret Key" name="secretKey" type="password" value="${store.secretKey ||
-                  ""}" />
-            </Container>
-            <Container>
-                <Button action="submit">Submit</Button>
-                <Button action="reset">Reset</Button>
-                <Button action="test">Test</Button>
-            </Container>
-        </Page>
-	`
+  return htm`
+		<Page>
+			<Container>
+				<Input label="Address Line 1" name="addressLine1" value=${store.addressLine1 ||
+          ""} />
+				<Input label="Address Line 2" name="addressLine2" value=${store.addressLine2 ||
+          ""} />
+				<Input label="City" name="city" value=${store.city || ""} />
+				<Input label="State" name="state" type="password" value=${store.state || ""} />
+				<Input label="Zip" name="zip" value=${store.zip || ""} />
+			</Container>
+			<Container>
+				<Button action="test">Test</Button>
+				<Button action="reset">Reset</Button>
+			</Container>
+		</Page>
+	`;
 });
 
-// async function setupWebhook(req, res) {
-// 	const {query} = parseUrl(req.url, true);
-// 	const tokenRes = await fetch('https://api.zeit.co/v2/oauth/access_token', {
-// 		method: 'POST',
-// 		headers: {
-// 			'Content-Type': 'application/x-www-form-urlencoded'
-// 		},
-// 		body: qs.stringify({
-// 			client_id: CLIENT_ID,
-// 			client_secret: CLIENT_SECRET,
-// 			code: query.code,
-// 			redirect_uri: REDIRECT_URL
-// 		})
-// 	});
-
-// 	const tokenPayload = await tokenRes.json();
-// 	const token = tokenPayload['access_token'];
-// 	TokenStore[query.configurationId] = token;
-
-// 	const zeitClient = new ZeitClient({token, teamId: query.teamId});
-// 	const hookInfo = await zeitClient.fetchAndThrow(`/v1/integrations/webhooks`, {
-// 		method: 'POST',
-// 		data: {
-// 			name: 'default',
-// 			url: `${ROOT_URL}/webhook`
-// 		}
-// 	});
-
-// 	res.writeHead(302, {
-// 		'Location': query.next
-// 	});
-// 	res.end('Redirecting..');
-// }
-
-// async function handleEvents(req, res) {
-// 	const event = await micro.json(req);
-// 	console.log('RECEIVE EVENT', event);
-// 	const ownerId = event.teamId || event.userId;
-
-// 	EventsStore[ownerId] = EventsStore[ownerId] || [];
-// 	EventsStore[ownerId].unshift(event);
-// 	EventsStore[ownerId] = EventsStore[ownerId].slice(0, 50);
-
-// 	return micro.send(res, 200, {});
-// }
-
 module.exports = function(req, res) {
-	// if (req.method === 'GET') {
-	// 	return setupWebhook(req, res);
-	// }
-
-	// if (req.url === '/webhook') {
-	// 	return handleEvents(req, res);
-	// }
-
-	return renderUIHook(req, res);
-}
+  return renderUIHook(req, res);
+};
